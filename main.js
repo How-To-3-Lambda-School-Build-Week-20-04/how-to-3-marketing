ca3da84f058c7ddfe4aca26df6187eb137dded7f
Andrew Shields <andrewshields@Andrews-iMac.attlocal.net>
2020-04-29 22:10:46 -0400

    added two slideshows of "content"

--- main.js
+++ main.js
@@ -14,4 +14,36 @@ anime.timeline({loop: true})
     duration: 1000,
     easing: "easeOutExpo",
     delay: 1000
-  });
+});
+
+
+
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("content");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {slideIndex = 1}
+  x[slideIndex-1].style.display = "block";
+  setTimeout(carousel, 5000);
+}
+
+var slideIndex2 = 0;
+carousel2();
+
+function carousel2() {
+  var i;
+  var x = document.getElementsByClassName("contents");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex2++;
+  if (slideIndex2 > x.length) {slideIndex2 = 1}
+  x[slideIndex2-1].style.display = "block";
+  setTimeout(carousel2, 6000); 
+}
